f7b415ad3c345397e34a4ec4479d85331e44cd03
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2024-04-21 12:40:00 +0100

    id added

--- todo.js
+++ todo.js
@@ -5,12 +5,21 @@ function renderTodo(todo) {
   const row = table.insertRow(-1);
   const textCell = row.insertCell(0);
   textCell.innerText = todo.text;
+  const dateCell = row.insertCell(1);
+  dateCell.innerText = todo.date;
 }
-
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 function addTodo() {
   const todoText = document.getElementById("todo-id").value;
   const todo = {
-    text: todoText
+    text: todoText,
+    date: new Date().toLocaleString("en-IE"),
+    id: uuidv4()
   };
   todoItems.push(todo);
   renderTodo(todo);
